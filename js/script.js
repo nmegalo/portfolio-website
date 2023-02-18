61c6594edb36cecde866705a3af3d4c86c8f6b22
nmegalo <123195650+nmegalo@users.noreply.github.com>
2023-02-17 21:51:29 -0500

    added function to toggle hamburger menu on/off

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("Welcome to my portfolio site! Take a look and let me know what you think!")
+
+//function to toggle menu off or on //
+
+function menuToggle (){
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
+
