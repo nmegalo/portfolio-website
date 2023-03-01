9e8040cc42f7c3879065a328508657a2fa917222
nmegalo <123195650+nmegalo@users.noreply.github.com>
2023-02-28 23:17:34 -0500

    changed button colors for better contrast

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ $('a[href*="#"]')
           } else {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
-          };
+          }
         });
       }
     }
